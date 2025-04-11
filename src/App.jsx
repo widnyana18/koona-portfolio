757717fa21940c75db79a0b6366ab3887f7cfb23
widnyana18 <mangwidnyana2d.art@gmail.com>
2025-04-12 06:31:17 +0800

    feat: add work project component

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Skill from "./components/Skill";
+import Project from "./components/Project";
 
 
 const App = () => {
@@ -18,6 +19,7 @@ const App = () => {
         <Hero />
         <About />
         <Skill />
+        <Project />
       </main>
     </>
   );
