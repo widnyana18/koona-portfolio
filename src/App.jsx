a692312d6c889925a70506b5b8ed05f4050a182d
widnyana18 <mangwidnyana2d.art@gmail.com>
2025-04-11 17:29:57 +0800

    feat: add about component

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,22 @@
  * @copyright 2025 komangwidnyana
  * @license Apache-2.0
  */
+
+// COMPONENTS
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import About from "./components/About";
 
 const App = () => {
   return (
-  <><Header /><main><Hero /></main></>
-);
+    <>
+      <Header />
+      <main>
+        <Hero />
+        <About />
+      </main>
+    </>
+  );
 };
 
 export default App;
